b8e05ecd3d4b0b413a35609049d47778b0a7dc0a
feliperafaldini <feliperafaldini@gmail.com>
2025-02-27 20:39:43 -0300

    Atualizações no código

--- scripts.js
+++ scripts.js
@@ -1,26 +1,6 @@
-document.getElementById('formulario-pet').addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    const nome = document.getElementById('nome').value;
-    const tipo = document.getElementById('tipo').value;
-    const raca = document.getElementById('raca').value;
-    const idade = document.getElementById('idade').value;
-    const tutor = document.getElementById('tutor').value;
-
-    const pet = {
-        nome,
-        tipo,
-        raca,
-        idade,
-        tutor,
-    };
-
-    document.getElementById('formulario-pet').reset();
-});
-
-document.getElementById('foto').addEventListener('change', function (event) {
+document.getElementById('photo').addEventListener('change', function (event) {
     const file = event.target.files[0];
-    const preview = document.getElementById('previa-foto');
+    const preview = document.getElementById('photo-preview');
 
     if (file) {
         const reader = new FileReader();
